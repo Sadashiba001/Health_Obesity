01dea534ecbfd1283c9622b14f77b1db02721a8b
Sadashiba001 <sadashibpanigrahy3@gmail.com>
2025-08-27 19:24:51 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("knn_heart_model.pkl")
+scaler = joblib.load("heart_scaler.pkl")
+expected_columns = joblib.load("heart_columns.pkl")
+
+st.title("Heart Stroke Prediction by Sadashib😊")
+st.markdown("Provide the following details to check your heart stroke risk:")
+
+st.markdown(
+    """
+    <style>
+        .stApp {
+            background-image: url("http://cdn.wallpapersafari.com/64/47/1e5hBX.jpg"); 
+            background-size: cover;
+        }
+
+        div.stButton > button:first-child {
+            background-color: #4CAF50;
+            color: white;
+            border-radius: 10px;
+            border: 2px solid #4CAF50;
+            height: 3em;
+            width: 10em;
+        }
+
+        div.stButton > button:hover {
+            background-color: white;
+            color: #4CAF50;
+            border: 2px solid #4CAF50;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+age = st.slider("Age", 18, 100, 40)
+sex = st.selectbox("Sex", ["M", "F"])
+chest_pain = st.selectbox("Chest Pain Type", ["ATA", "NAP", "TA", "ASY"])
+resting_bp = st.number_input("Resting Blood Pressure (mm Hg)", 80, 200, 120)
+cholesterol = st.number_input("Cholesterol (mg/dL)", 100, 600, 200)
+fasting_bs = st.selectbox("Fasting Blood Sugar > 120 mg/dL", [0, 1])
+resting_ecg = st.selectbox("Resting ECG", ["Normal", "ST", "LVH"])
+max_hr = st.slider("Max Heart Rate", 60, 220, 150)
+exercise_angina = st.selectbox("Exercise-Induced Angina", ["Y", "N"])
+oldpeak = st.slider("Oldpeak (ST Depression)", 0.0, 6.0, 1.0)
+st_slope = st.selectbox("ST Slope", ["Up", "Flat", "Down"])
+
+if st.button("Predict"):
+
+    raw_input = {
+        'Age': age,
+        'RestingBP': resting_bp,
+        'Cholesterol': cholesterol,
+        'FastingBS': fasting_bs,
+        'MaxHR': max_hr,
+        'Oldpeak': oldpeak,
+        'Sex_' + sex: 1,
+        'ChestPainType_' + chest_pain: 1,
+        'RestingECG_' + resting_ecg: 1,
+        'ExerciseAngina_' + exercise_angina: 1,
+        'ST_Slope_' + st_slope: 1
+    }
+
+    input_df = pd.DataFrame([raw_input])
+
+    for col in expected_columns:
+        if col not in input_df.columns:
+            input_df[col] = 0
+
+    input_df = input_df[expected_columns]
+
+    scaled_input = scaler.transform(input_df)
+
+    prediction = model.predict(scaled_input)[0]
+
+    if prediction == 1:
+        st.error("⚠️ High Risk of Heart Disease")
+    else:
+        st.success("✅ Low Risk of Heart Disease")
